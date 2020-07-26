281e6e1d65478c80ddc750a9c86445af6c725782
VICDATA-A <tmdrb991@gmail.com>
2020-07-26 20:00:32 +0900

    firstCommit

---
+++ JumpToPython/list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 문자열 곱하기 응용
+
+# In[2]:
+
+
+print("="*50)
+print("My program")
+print("="*50)
+
+
+# In[10]:
+
+
+a = "Life is too short"
+len(a)
+
+
+# In[11]:
+
+
+"Hello World"
+
+
+# 문자열 곱하기 응용
+
+# In[ ]:
+
+
+
+
