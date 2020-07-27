3d46c8d099fa8ca02b4f30f06ac5676f5d20d839
VICDATA-A <tmdrb991@gmail.com>
2020-07-27 14:02:52 +0900

    git-python

---
+++ JumpToPython/문자열 자료형.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 문자형 자료형
+
+# In[1]:
+
+
+" Hello World"
+
+
+# In[2]:
+
+
+' Python is fun'
+
+
+# In[3]:
+
+
+"""Life is too short, you need python"""
+
+
+# In[4]:
+
+
+'''Life is too short, you need python'''
+
+
+# In[5]:
+
+
+food = "Python's favorite food is perl"
+
+
+# In[6]:
+
+
+food = 'Python's favorite food is perl'
+
+
+# In[7]:
+
+
+say = '"Python is vert easy." he says.'
+
+
+# In[8]:
+
+
+food = 'Python\'s favorite food is perl'
+
+
+# In[10]:
+
+
+say = "\"Pyhon is very easy.\"he says."
+
+
+# In[16]:
+
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+
+# In[13]:
+
+
+multiline="""
+    Life is too short
+    You need python
+    """
+
+
+# In[14]:
+
+
+print(multiline)
+
+
+# In[17]:
+
+
+head = "Python"
+tail = "is fun!"
+head + tail
+
+
+# In[19]:
+
+
+a = "Python"
+a * 2
+
+
+# In[20]:
+
+
+a = "Life is too short"
+len(a)
+
+
+# In[21]:
+
+
+a = "Life is too short, You need Python"
+
+
+# In[22]:
+
+
+a = "Life is too short, You need Pyhon"
+a[3]
+
+
+# In[25]:
+
+
+a = "Life is too short, You need Python"
+a[0]
+
+
+# In[29]:
+
+
+a = "Life is too short, You need Python"
+b = a[0] + a[1] + a[2] + a[3]
+b
+
+
+# In[30]:
+
+
+a = "Life is too short, You need Python"
+a[0:4]
+
+
+# In[31]:
+
+
+a = "Life is too short, You need Python"
+a[19:]
+
+
+# In[34]:
+
+
+a = "Life is too short, You need Python"
+a[:]
+
+
+# In[35]:
+
+
+a[19 : -7]
+
+
+# In[38]:
+
+
+a = "20010331Rainy"
+date = a[:8]
+date
+
+
+# In[40]:
+
+
+a = "20010331Rainy"
+weather = a[8:]
+weather
+
+
+# In[42]:
+
+
+a = "20010331Rainy"
+year = a[:4]
+day = a[4:8]
+weather = a[8:]
+year
+
+
+# In[43]:
+
+
+weather
+
+
+# In[45]:
+
+
+day
+
+
+# In[46]:
+
+
+a = "Pithon"
+a[1]
+
+
+# In[49]:
+
+
+a[1] = 'y'
+
+
+# In[50]:
+
+
+a = "Pition"
+a[:1]
+
+
+# In[51]:
+
+
+a[2:]
+
+
+# In[57]:
+
+
+a[:1] + 'y' + a[2] + 'h' + a[4:]
+
+
+# In[ ]:
+
+
+
+
