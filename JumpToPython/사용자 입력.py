b9392bdc1fc556aa8db83fc1772afbf45755cd6c
VICDATA-A <tmdrb991@gmail.com>
2020-08-06 17:02:23 +0900

    사용자입력연습

---
+++ JumpToPython/사용자 입력.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 사용자 입력
+
+# In[20]:
+
+
+a = input()
+Life is too short, you need python
+a
+
+
+# In[21]:
+
+
+number = input("숫자를 입력하세요: ")
+
+
+# In[22]:
+
+
+number = input("숫자를 입력하세요: ")
+
+
+# In[24]:
+
+
+print(number)
+
+
+# In[25]:
+
+
+a = 123
+print(a)
+
+
+# In[27]:
+
+
+a = 'python'
+print(a)
+
+
+# In[30]:
+
+
+# 큰 따오표("")로 둘러싸인 문자열은 + 연산과 동일
+print("life" "is" "too short")
+
+
+# In[31]:
+
+
+# 문자열의 띄어쓰기는 콤마(,)로 한다.
+print("life", "is", "too short")
+
+
+# In[35]:
+
+
+# for문에서 했듯이 결과값을 한 줄에 계속 이어서 출력하려면 매개변수 end를 사용해 끝 문자를 지정해야 한다.
+for i in range(10):
+    print(i, end='')
+
+
+# In[ ]:
+
+
+
+
